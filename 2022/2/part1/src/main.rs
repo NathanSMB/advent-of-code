ae855bc39f3f9646effd08483c59ed537329f424
NathanSMB <nathan@burchill.io>
2022-12-02 00:56:22 -0500

    Day two solutions

---
+++ 2022/2/part1/src/main.rs
@@ -0,0 +1,75 @@
+use std::fs;
+
+enum RockPaperScissorSelection {
+    Rock,
+    Paper,
+    Scissor,
+}
+
+struct RoundStrategy {
+    opponent: RockPaperScissorSelection,
+    myself: RockPaperScissorSelection,
+}
+
+fn play_and_score(round: RoundStrategy, score: &mut i32) {
+    let win_points = 6;
+    let tie_points = 3;
+    let lose_points = 0;
+
+    match round.myself {
+        RockPaperScissorSelection::Rock => {
+            *score += 1;
+            match round.opponent {
+                RockPaperScissorSelection::Rock => *score += tie_points,
+                RockPaperScissorSelection::Paper => *score += lose_points,
+                RockPaperScissorSelection::Scissor => *score += win_points,
+            }
+        },
+        RockPaperScissorSelection::Paper => {
+            *score += 2;
+            match round.opponent {
+                RockPaperScissorSelection::Rock => *score += win_points,
+                RockPaperScissorSelection::Paper => *score += tie_points,
+                RockPaperScissorSelection::Scissor => *score += lose_points,
+            }
+        },
+        RockPaperScissorSelection::Scissor => {
+            *score += 3;
+            match round.opponent {
+                RockPaperScissorSelection::Rock => *score += lose_points,
+                RockPaperScissorSelection::Paper => *score += win_points,
+                RockPaperScissorSelection::Scissor => *score += tie_points,
+            }
+        },
+    }
+}
+
+fn main() {
+    let input = fs::read_to_string("input").expect("Error reading input file");
+    let input: Vec<&str> = input.lines().collect();
+    let input: Vec<RoundStrategy> = input
+        .iter()
+        .map(|line| {
+            let mut line = line.split_whitespace();
+            let opponent = match line.next().unwrap() {
+                "A" => RockPaperScissorSelection::Rock,
+                "B" => RockPaperScissorSelection::Paper,
+                "C" => RockPaperScissorSelection::Scissor,
+                _ => panic!("Invalid input"),
+            };
+            let myself = match line.next().unwrap() {
+                "X" => RockPaperScissorSelection::Rock,
+                "Y" => RockPaperScissorSelection::Paper,
+                "Z" => RockPaperScissorSelection::Scissor,
+                _ => panic!("Invalid input"),
+            };
+            RoundStrategy { opponent, myself }
+        })
+        .collect();
+
+    let mut my_score = 0;
+    for round in input {
+        play_and_score(round, &mut my_score);
+    }
+    println!("{}", my_score);
+}
